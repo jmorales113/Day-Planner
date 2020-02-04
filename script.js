ef5c87a8033996514bf05776bf8afba69f2938a6
Jorge Morales <jmorales113@gmail.com>
2020-02-03 16:38:43 -0800

    Add save button function.

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
     var dateField = $("#currentDayTime")
     dateField.text(currentDate)
 
-})
+    })
+
+    $(".saveButton").click(function(){
+        document.clear()
+        location.reload()
+    })
+
+    var currentDateTime = new Date().getHours()
+
+    if (currentDateTime > 9) {
+        $("#inputField").addClass("pastTime")
+
+        } else if (currentDateTime >= 9 && currentDateTime < 10) {
+            $("#inputField").addClass("presentTime")
+
+        } else if (currentDateTime < 9) {
+            $("#inputField").addClass("futureTime")
+        }
+        
